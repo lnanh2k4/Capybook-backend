8c70597d140b41b462607d6bac9774ae07920dc9
lnanh2k4 <lnanh2k4@gmail.com>
2024-10-09 22:11:39 +0700

    first commit

---
+++ src/main/java/fa24/swp391/se1802/group3/capybook/CapybookApplication.java
@@ -0,0 +1,13 @@
+package fa24.swp391.se1802.group3.capybook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapybookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapybookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fa24/swp391/se1802/group3/capybook/CapybookApplicationTests.java
@@ -0,0 +1,13 @@
+package fa24.swp391.se1802.group3.capybook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapybookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
