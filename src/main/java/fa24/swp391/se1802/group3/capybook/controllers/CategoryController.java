d41561989edae52f4994ee1cdc27b7daf16388e7
Duy Tin <tinnldce170649@fpt.edu.vn>
2024-10-12 15:14:29 +0700

    abcd

--- src/main/java/fa24/swp391/se1802/group3/capybook/controllers/CategoryController.java
+++ src/main/java/fa24/swp391/se1802/group3/capybook/controllers/CategoryController.java
@@ -1,13 +1,27 @@
 package fa24.swp391.se1802.group3.capybook.controllers;
 
+import fa24.swp391.se1802.group3.capybook.daos.CategoryDAO;
+import fa24.swp391.se1802.group3.capybook.models.CategoryDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/v1/category")
+@RequestMapping("/api/v1/categories")
 public class CategoryController {
-    @RequestMapping("/")
     public String getCategoryList(){
-        return "category list";
-    }
+        CategoryDAO categoryDAO;
+
+    @Autowired
+    public CategoryController(CategoryDAO categoryDAO) {
+            this.categoryDAO = categoryDAO;
+        }
+
+        @GetMapping("/")
+        public List<CategoryDTO> getCategorissList(){
+            return categoryDAO.findAll();
+        }
 }
