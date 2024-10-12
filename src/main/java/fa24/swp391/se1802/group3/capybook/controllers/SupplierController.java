6358154de2226e3254d8482accfd1ee1d88e54d1
VuNLK <vunlkce180175@fpt.edu.vn>
2024-10-12 15:10:50 +0700

    Update Controller Supplier

--- src/main/java/fa24/swp391/se1802/group3/capybook/controllers/SupplierController.java
+++ src/main/java/fa24/swp391/se1802/group3/capybook/controllers/SupplierController.java
@@ -1,4 +1,29 @@
 package fa24.swp391.se1802.group3.capybook.controllers;
 
+import fa24.swp391.se1802.group3.capybook.daos.AccountDAO;
+import fa24.swp391.se1802.group3.capybook.daos.SupplierDAO;
+import fa24.swp391.se1802.group3.capybook.models.AccountDTO;
+import fa24.swp391.se1802.group3.capybook.models.SupplierDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/suppliers")
 public class SupplierController {
+    SupplierDAO supplierDAO;
+
+    @Autowired
+    public SupplierController(SupplierDAO supplierDAO) {
+        this.supplierDAO = supplierDAO;
+    }
+
+    @GetMapping("/")
+    public List<SupplierDTO> getSuppliersList() {
+        return supplierDAO.findAll();
+    }
 }
+
