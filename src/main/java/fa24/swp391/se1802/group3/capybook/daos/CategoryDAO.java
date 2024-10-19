ce6fe9ae6761feadaff85b78306f343460d4973c
Khanh <khanhdcce180117@fpt.edu.vn>
2024-10-19 21:43:19 +0700

    Finish CRUD Category

--- src/main/java/fa24/swp391/se1802/group3/capybook/daos/CategoryDAO.java
+++ src/main/java/fa24/swp391/se1802/group3/capybook/daos/CategoryDAO.java
@@ -1,7 +1,6 @@
 package fa24.swp391.se1802.group3.capybook.daos;
 
 import fa24.swp391.se1802.group3.capybook.models.CategoryDTO;
-
 import java.util.List;
 
 public interface CategoryDAO {
@@ -9,4 +8,10 @@ public interface CategoryDAO {
     CategoryDTO find(int catID);
     void delete(int catID);
     List<CategoryDTO> findAll();
+
+    // Thêm phương thức này để tìm các category con
+    List<CategoryDTO> findByParentCatID(int parentCatID);
+
+    // Thêm phương thức này để tìm kiếm category theo tên
+    List<CategoryDTO> searchByName(String name);
 }
--- src/main/java/fa24/swp391/se1802/group3/capybook/daos/CategoryDAOlmpl.java
+++ src/main/java/fa24/swp391/se1802/group3/capybook/daos/CategoryDAOlmpl.java
@@ -1,6 +1,5 @@
 package fa24.swp391.se1802.group3.capybook.daos;
 
-
 import fa24.swp391.se1802.group3.capybook.exceptions.CategoryExceptionNotFound;
 import fa24.swp391.se1802.group3.capybook.models.CategoryDTO;
 import jakarta.persistence.EntityManager;
@@ -8,7 +7,6 @@ import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 
 @Repository
@@ -16,30 +14,54 @@ public class CategoryDAOlmpl implements CategoryDAO {
     EntityManager entityManager;
 
     @Autowired
-    public CategoryDAOlmpl(EntityManager entityManager){
+    public CategoryDAOlmpl(EntityManager entityManager) {
         this.entityManager = entityManager;
     }
+
     @Override
     @Transactional
-    public CategoryDTO save(CategoryDTO categoryDTO){
+    public CategoryDTO save(CategoryDTO categoryDTO) {
+        if (categoryDTO.getCatName() == null || categoryDTO.getCatName().isEmpty()) {
+            throw new IllegalArgumentException("Category name cannot be null or empty");
+        }
         return entityManager.merge(categoryDTO);
     }
+
     @Override
-    public CategoryDTO find(int catID){
-        CategoryDTO object = entityManager.find(CategoryDTO.class,catID);
-        if (object!= null){
+    public CategoryDTO find(int catID) {
+        CategoryDTO object = entityManager.find(CategoryDTO.class, catID);
+        if (object != null) {
             return object;
         } else {
             throw new CategoryExceptionNotFound();
         }
     }
+
     @Override
     @Transactional
-    public void delete(int catID){
+    public void delete(int catID) {
         entityManager.remove(this.find(catID));
     }
-    public List<CategoryDTO> findAll(){
-        TypedQuery<CategoryDTO> query = entityManager.createQuery("From CategoryDTO",CategoryDTO.class);
+
+    @Override
+    public List<CategoryDTO> findAll() {
+        TypedQuery<CategoryDTO> query = entityManager.createQuery("SELECT c FROM CategoryDTO c", CategoryDTO.class);
+        return query.getResultList();
+    }
+
+    @Override
+    public List<CategoryDTO> findByParentCatID(int parentCatID) {
+        TypedQuery<CategoryDTO> query = entityManager.createQuery(
+                "SELECT c FROM CategoryDTO c WHERE c.parentCatID = :parentCatID", CategoryDTO.class);
+        query.setParameter("parentCatID", parentCatID);
+        return query.getResultList();
+    }
+
+    @Override
+    public List<CategoryDTO> searchByName(String name) {
+        TypedQuery<CategoryDTO> query = entityManager.createQuery(
+                "SELECT c FROM CategoryDTO c WHERE LOWER(c.catName) LIKE LOWER(:name)", CategoryDTO.class);
+        query.setParameter("name", "%" + name + "%");
         return query.getResultList();
     }
 }
