e0004e0a15033f0ccf211984b21ffa033650a659
TyHH <mrmastervp123@gmail.com>
2024-10-19 11:34:23 +0700

    Fix BookDTO

--- src/main/java/fa24/swp391/se1802/group3/capybook/models/BookDTO.java
+++ src/main/java/fa24/swp391/se1802/group3/capybook/models/BookDTO.java
@@ -19,7 +19,6 @@ import java.util.Collection;
 @Entity
 @Table(name = "book")
 public class BookDTO implements Serializable {
-    //Define fields for book class
     @Serial
     private static final long serialVersionUID = 1L;
     @Id
@@ -27,62 +26,45 @@ public class BookDTO implements Serializable {
     @Basic(optional = false)
     @Column(name = "bookid")
     private Integer bookID;
-
     @Column(name = "booktitle")
     private String bookTitle;
-
     @Column(name = "author")
     private String author;
-
     @Column(name = "translator")
     private String translator;
-
     @Column(name = "publisher")
     private String publisher;
-
     @Column(name = "publicationyear")
     private Integer publicationYear;
-
     @Column(name = "isbn")
     private String isbn;
-
     @Lob
     @Column(name = "image")
     private String image;
-
     @Lob
     @Column(name = "bookdescription")
     private String bookDescription;
-
     @Column(name = "hardcover")
     private Integer hardcover;
-
     @Column(name = "dimension")
     private String dimension;
-
     // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
     @Column(name = "weight")
     private Double weight;
-
     @Column(name = "bookprice")
     private BigDecimal bookPrice;
-
     @Column(name = "bookquantity")
     private Integer bookQuantity;
-
     @Column(name = "bookstatus")
     private Integer bookStatus;
-
     @OneToMany(mappedBy = "bookID", fetch = FetchType.LAZY)
     @JsonManagedReference
     @JsonIgnore
     private Collection<OrderDetailDTO> orderDetailCollection;
-
     @OneToMany(mappedBy = "bookID", fetch = FetchType.LAZY)
     @JsonManagedReference
     @JsonIgnore
     private Collection<ImportStockDetailDTO> importStockDetailCollection;
-
     @JoinColumn(name = "catid", referencedColumnName = "catid")
     @ManyToOne(fetch = FetchType.LAZY)
     @JsonBackReference
@@ -91,6 +73,7 @@ public class BookDTO implements Serializable {
 
     @OneToMany(mappedBy = "bookID")
     @JsonManagedReference("book-cart")
+    @JsonIgnore
     private Collection<CartDTO> cartCollection;
 
 
