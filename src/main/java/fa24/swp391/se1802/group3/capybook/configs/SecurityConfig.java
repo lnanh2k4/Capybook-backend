6cef6aae86338c64b1a86b99e36823ae1ce9cf08
lnanh2k4 <lnanh2k4@gmail.com>
2024-11-21 04:28:45 +0700

    final

--- src/main/java/fa24/swp391/se1802/group3/capybook/configs/SecurityConfig.java
+++ src/main/java/fa24/swp391/se1802/group3/capybook/configs/SecurityConfig.java
@@ -27,11 +27,47 @@ import java.util.Arrays;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
- private final String[] PUBLIC_ENDPOINTS = {"/api/auth/token", "/api/auth/introspect","/api/auth/logout","/api/auth/refresh","/api/v1/accounts/register" }; // url cho guest
-    private final String[] SELLER_STAFF_ENDPOINTS = {"/auth/token","/api/v1/accounts/", "/auth/introspect"}; // url cho seller
-    private final String[] WAREHOUSE_STAFF_ENDPOINTS = {"/auth/token","/api/v1/accounts/", "/auth/introspect"}; // url cho warehouse
-    private final String[] ADMIN_ENDPOINTS = {"/auth/token","/api/v1/accounts/", "/auth/introspect"}; // url cho admin
-    private final String[] CUSTOMER_ENDPOINTS = {"/auth/token","/api/v1/accounts/", "/auth/introspect"}; // url cho customer
+ private final String[] PUBLIC_ENDPOINTS = {"/api/auth/token",
+         "/api/auth/introspect",
+         "/api/auth/logout",
+         "/api/auth/refresh",
+         "/api/v1/accounts/register" ,"/api/v1/accounts/","/api/v1/accounts/{username}", "/api/v1/accounts/search", "/api/v1/accounts/change"
+         ,"/api/v1/books/", "/api/v1/books/{bookId}",
+    "/api/v1/cart","/api/v1/cart/add","/api/v1/cart/update", "/api/v1/cart/delete",
+         "/api/v1/categories","/api/v1/categories/{catID}", "/api/v1/categories/{catID}/soft-delete", "/api/v1/categories/search",
+         "api/v1/importStock",
+         "/api/v1/importStock/{id}/details",
+         "/api/v1/notifications","/api/v1/notifications/detail/{notID}", "/api/v1/notifications/{notTitle}", "/api/v1/notifications/{notID}",
+         "/api/v1/orders","/api/v1/orders/{orderID}", "/api/v1/orders/search",
+         "/api/v1/payment", "/api/v1/payment/create", "/api/v1/payment/return",
+         "/api/v1/promotions", "/api/v1/promotions/{proID}", "/api/v1/promotions/search",
+         "/api/v1/staff", "/api/v1/staff/{id}",
+         "api/v1/suppliers", "api/v1/suppliers/{id}", "api/v1/suppliers/{supID}", "api/v1/suppliers/search"
+ }; // url cho guest
+    private final String[] SELLER_STAFF_ENDPOINTS = {"/api/auth/token",
+            "/api/auth/introspect",
+            "/api/auth/logout",
+            "/api/auth/refresh",
+            "/api/v1/accounts/register" ,"/api/v1/accounts/","/api/v1/accounts/{username}"
+            ,"/api/v1/books/"}; // url cho seller
+    private final String[] WAREHOUSE_STAFF_ENDPOINTS = {"/api/auth/token",
+            "/api/auth/introspect",
+            "/api/auth/logout",
+            "/api/auth/refresh",
+            "/api/v1/accounts/register" ,"/api/v1/accounts/","/api/v1/accounts/{username}"
+            ,"/api/v1/books/"}; // url cho warehouse
+    private final String[] ADMIN_ENDPOINTS = {"/api/auth/token",
+            "/api/auth/introspect",
+            "/api/auth/logout",
+            "/api/auth/refresh",
+            "/api/v1/accounts/register" ,"/api/v1/accounts/","/api/v1/accounts/{username}"
+            ,"/api/v1/books/"}; // url cho admin
+    private final String[] CUSTOMER_ENDPOINTS = {"/api/auth/token",
+            "/api/auth/introspect",
+            "/api/auth/logout",
+            "/api/auth/refresh",
+            "/api/v1/accounts/register" ,"/api/v1/accounts/","/api/v1/accounts/{username}"
+            ,"/api/v1/books/"}; // url cho customer
 
     @Autowired
     private CustomerJwtDecoder customerJwtDecoder;
@@ -45,26 +81,27 @@ public class SecurityConfig {
                         request
                                 .requestMatchers(HttpMethod.POST,PUBLIC_ENDPOINTS).permitAll()
                                 .requestMatchers(HttpMethod.GET,PUBLIC_ENDPOINTS).permitAll()
+                                .requestMatchers(HttpMethod.PUT,PUBLIC_ENDPOINTS).permitAll()
 
-                                .requestMatchers(HttpMethod.GET,ADMIN_ENDPOINTS).hasAnyAuthority("SCOPE_ADMIN")
-                                .requestMatchers(HttpMethod.POST,ADMIN_ENDPOINTS).hasAnyAuthority("SCOPE_ADMIN")
-                                .requestMatchers(HttpMethod.PUT,ADMIN_ENDPOINTS).hasAnyAuthority("SCOPE_ADMIN")
-                                .requestMatchers(HttpMethod.DELETE,ADMIN_ENDPOINTS).hasAnyAuthority("SCOPE_ADMIN")
+                                .requestMatchers(HttpMethod.GET,PUBLIC_ENDPOINTS).hasAnyAuthority("SCOPE_ADMIN")
+                                .requestMatchers(HttpMethod.POST,PUBLIC_ENDPOINTS).hasAnyAuthority("SCOPE_ADMIN")
+                                .requestMatchers(HttpMethod.PUT,PUBLIC_ENDPOINTS).hasAnyAuthority("SCOPE_ADMIN")
+                                .requestMatchers(HttpMethod.DELETE,PUBLIC_ENDPOINTS).hasAnyAuthority("SCOPE_ADMIN")
 
-                                .requestMatchers(HttpMethod.POST,CUSTOMER_ENDPOINTS).hasAnyAuthority("SCOPE_CUSTOMER")
-                                .requestMatchers(HttpMethod.GET,CUSTOMER_ENDPOINTS).hasAnyAuthority("SCOPE_CUSTOMER")
-                                .requestMatchers(HttpMethod.PUT,CUSTOMER_ENDPOINTS).hasAnyAuthority("SCOPE_CUSTOMER")
-                                .requestMatchers(HttpMethod.DELETE,CUSTOMER_ENDPOINTS).hasAnyAuthority("SCOPE_CUSTOMER")
+                                .requestMatchers(HttpMethod.POST,PUBLIC_ENDPOINTS).hasAnyAuthority("SCOPE_CUSTOMER")
+                                .requestMatchers(HttpMethod.GET,PUBLIC_ENDPOINTS).hasAnyAuthority("SCOPE_CUSTOMER")
+                                .requestMatchers(HttpMethod.PUT,PUBLIC_ENDPOINTS).hasAnyAuthority("SCOPE_CUSTOMER")
+                                .requestMatchers(HttpMethod.DELETE,PUBLIC_ENDPOINTS).hasAnyAuthority("SCOPE_CUSTOMER")
 
-                                .requestMatchers(HttpMethod.POST,SELLER_STAFF_ENDPOINTS).hasAnyAuthority("SCOPE_SELLER_STAFF")
-                                .requestMatchers(HttpMethod.GET,SELLER_STAFF_ENDPOINTS).hasAnyAuthority("SCOPE_SELLER_STAFF")
-                                .requestMatchers(HttpMethod.PUT,SELLER_STAFF_ENDPOINTS).hasAnyAuthority("SCOPE_SELLER_STAFF")
-                                .requestMatchers(HttpMethod.DELETE,SELLER_STAFF_ENDPOINTS).hasAnyAuthority("SCOPE_SELLER_STAFF")
+                                .requestMatchers(HttpMethod.POST,PUBLIC_ENDPOINTS).hasAnyAuthority("SCOPE_SELLER_STAFF")
+                                .requestMatchers(HttpMethod.GET,PUBLIC_ENDPOINTS).hasAnyAuthority("SCOPE_SELLER_STAFF")
+                                .requestMatchers(HttpMethod.PUT,PUBLIC_ENDPOINTS).hasAnyAuthority("SCOPE_SELLER_STAFF")
+                                .requestMatchers(HttpMethod.DELETE,PUBLIC_ENDPOINTS).hasAnyAuthority("SCOPE_SELLER_STAFF")
 
-                                .requestMatchers(HttpMethod.POST,WAREHOUSE_STAFF_ENDPOINTS).hasAnyAuthority("SCOPE_WAREHOUSE_STAFF")
-                                .requestMatchers(HttpMethod.GET,WAREHOUSE_STAFF_ENDPOINTS).hasAnyAuthority("SCOPE_WAREHOUSE_STAFF")
-                                .requestMatchers(HttpMethod.PUT,WAREHOUSE_STAFF_ENDPOINTS).hasAnyAuthority("SCOPE_WAREHOUSE_STAFF")
-                                .requestMatchers(HttpMethod.DELETE,WAREHOUSE_STAFF_ENDPOINTS).hasAnyAuthority("SCOPE_WAREHOUSE_STAFF")
+                                .requestMatchers(HttpMethod.POST,PUBLIC_ENDPOINTS).hasAnyAuthority("SCOPE_WAREHOUSE_STAFF")
+                                .requestMatchers(HttpMethod.GET,PUBLIC_ENDPOINTS).hasAnyAuthority("SCOPE_WAREHOUSE_STAFF")
+                                .requestMatchers(HttpMethod.PUT,PUBLIC_ENDPOINTS).hasAnyAuthority("SCOPE_WAREHOUSE_STAFF")
+                                .requestMatchers(HttpMethod.DELETE,PUBLIC_ENDPOINTS).hasAnyAuthority("SCOPE_WAREHOUSE_STAFF")
 
                                 .anyRequest().authenticated());
 
